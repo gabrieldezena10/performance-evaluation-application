c87803fa73d5984fef9cd2dac13c38e33090e1d6
gabrieldezena10 <gabriel.deztor@gmail.com>
2024-06-03 21:35:30 -0300

    test: add user entity get methods test cases

--- apps/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ apps/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,24 @@ describe('User entity unit tests', () => {
     expect(user.properties.password).toBe(userProperties.password);
     expect(user.properties.createdAt).toBeInstanceOf(Date);
   });
+
+  it('Get name method', () => {
+    expect(user.name).toBe(userProperties.name);
+    expect(typeof user.name).toBe('string');
+  });
+
+  it('Get email method', () => {
+    expect(user.email).toBe(userProperties.email);
+    expect(typeof user.email).toBe('string');
+  });
+
+  it('Get password method', () => {
+    expect(user.password).toBe(userProperties.password);
+    expect(typeof user.password).toBe('string');
+  });
+
+  it('Get createdAt method', () => {
+    expect(user.createdAt).toBe(userProperties.createdAt);
+    expect(user.createdAt).toBeInstanceOf(Date);
+  });
 });
