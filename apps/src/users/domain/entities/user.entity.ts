4c51796852c66b91595e8043ff72c472593377a5
gabrieldezena10 <gabriel.deztor@gmail.com>
2024-06-03 21:34:55 -0300

    feat: add user entity get methods

--- apps/src/users/domain/entities/user.entity.ts
+++ apps/src/users/domain/entities/user.entity.ts
@@ -9,4 +9,19 @@ export class UserEntity {
   constructor(public readonly properties: IUserProperties) {
     this.properties.createdAt = this.properties.createdAt ?? new Date();
   }
+  get name(): string {
+    return this.properties.name;
+  }
+
+  get email(): string {
+    return this.properties.email;
+  }
+
+  get password(): string {
+    return this.properties.password;
+  }
+
+  get createdAt(): Date {
+    return this.properties.createdAt;
+  }
 }
